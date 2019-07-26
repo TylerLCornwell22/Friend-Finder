2d92832f23fbf86f1416973b2cb4524b12afc866
Tyler Cornwell <tylercornwell@hotmail.com>
2019-07-25 18:53:47 -0700

    friend finder hw

---
+++ app/routes/apiRoutes.js
@@ -0,0 +1,54 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var frienddata = require("../data/friends");
+
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(frienddata);
+  });
+
+  app.get("/api/waitlist", function(req, res) {
+    res.json(waitListData);
+  });
+
+  
+  app.post("/api/tables", function(req, res) {
+   
+    if (frienddata.length < 5) {
+      frienddata.push(req.body);
+      res.json(true);
+    }
+    else {
+      waitListData.push(req.body);
+      res.json(false);
+    }
+  });
+
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+  app.post("/api/clear", function(req, res) {
+    // Empty out the arrays of data
+    frienddata.length = 0;
+    waitListData.length = 0;
+
+    res.json({ ok: true });
+  });
+};
